1e3e184e1bd2d9471b2aa602fb0e2cb57f6cdbb0
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-14 18:59:38 +0530

    Update wargames.py

--- War Games/wargames.py
+++ War Games/wargames.py
@@ -1,32 +1,47 @@
-# We have to check whether count is less than 82 to prevent infinite loop.
-# I do not know the significance of the value 82. I just found it by trial and error.
+def play_game(deck1, deck2):
+    seen = set()
+    while deck1 and deck2:
+        current_state = (tuple(deck1), tuple(deck2))
+        if current_state in seen:
+            return "draw"
+        seen.add(current_state)
 
-values = {"2":1, "3":2, "4":3, "5":4, "6":5, "7":6, "8":7, "9":8, "T":9, "J":10, "Q":11, "K":12, "A":13}
+        card1 = deck1.pop(0)
+        card2 = deck2.pop(0)
 
-for x in range(int(input())):
-    player_1 = input().split()
-    player_2 = input().split()
+        if values[card1] > values[card2]:
+            deck1.append(card1)
+            deck1.append(card2)
+        elif values[card2] > values[card1]:
+            deck2.append(card2)
+            deck2.append(card1)
+        else:
+            deck1.append(card1)
+            deck2.append(card2)
+
+    if not deck1:
+        return "player 2"
+    elif not deck2:
+        return "player 1"
+
+values = {"2": 1, "3": 2, "4": 3, "5": 4, "6": 5, "7": 6, "8": 7, "9": 8, "T": 9, "J": 10, "Q": 11, "K": 12, "A": 13}
+
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().splitlines()
     
-    count = 0
-    
-    while len(player_1) > 0 and len(player_2) > 0 and player_1 != player_2 and count < 82:
-        card_1 = player_1.pop(0)
-        card_2 = player_2.pop(0)
+    n = int(data[0].strip())
+    if 1 <= n <= 25:
+        results = []
+        for i in range(1, 2*n, 2):
+            deck1 = data[i].strip().split()
+            deck2 = data[i+1].strip().split()
+            result = play_game(deck1, deck2)
+            results.append(result)
         
-        if values[card_1] > values[card_2]:
-            player_1.append(card_2)
-        elif values[card_1] < values[card_2]:
-            player_2.append(card_1)
-        else:
-            player_1.append(card_1)
-            player_2.append(card_2)
-            
-        count += 1
-            
-    else:
-        if len(player_1) == 0:
-            print("player 2")
-        elif len(player_2) == 0:
-            print("player 1")
-        else:
-            print("draw")
+        for result in results:
+            print(result)
+
+if __name__ == "__main__":
+    main()
