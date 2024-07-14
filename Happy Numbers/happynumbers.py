365834e37584f25222cfe7ad813e9d5000b12689
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-14 19:02:18 +0530

    Update happynumbers.py

--- Happy Numbers/happynumbers.py
+++ Happy Numbers/happynumbers.py
@@ -1,1 +1,21 @@
+def is_happy(num): # num is the number to check if it is happy
+    seen = set()
+    while num != 1 and num not in seen:
+        seen.add(num)
+        num = sum(int(digit) ** 2 for digit in str(num))
+    return num == 1
 
+def count_happy_numbers(a, b):
+    
+    count = 0
+    for num in range(a, b + 1):
+        if is_happy(num):
+            count += 1
+    return count
+    
+str1 = input()
+a, b = str1.split(' ')
+a = int(a)
+b = int(b)
+happy_count=count_happy_numbers(a,b)
+print(happy_count)
